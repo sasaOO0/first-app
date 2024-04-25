fc5f0e1bf6182a1df098182e8885a3a06ffd6c25
sasaOO0 <mustafaalfaki841@gmail.com>
2024-04-25 17:58:00 +0200

    sasa

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,16 +1,16 @@
 function App() {
   return (
-    <div className="App">
-      <h1>sasa 2</h1>
-      <h1>sasa 2</h1>
-      <h1>sasa 2</h1>
-      <h1>sasa 2</h1>
-      <h1>sasa 2</h1>
-      <h1>sasa 2</h1>
+    <div className="App"> 
+      <h1>sasa 88</h1>
+      <h1>sasa 88</h1>
+      <h1>sasa 88</h1>
+      <h1>sasa 88</h1>
+      <h1>sasa 88</h1>
       <h1 className="App-header">Hello World!</h1>
       <h1>Hello World!</h1>
     </div>
   );
 }
+
 
 export default App;
